a5c28e119118272ee75fe9ee699fffbc4150e667
Marlon Munoz <marlon@marlonshade.com>
2024-09-10 16:52:44 -0400

    sound path fixed

--- cli.py
+++ cli.py
@@ -36,7 +36,7 @@ def add_score(username, score, player_choices):
 
 
 
-SOUND_DIR = "/Users/Marlon/Development/code/surviving_nyc/shade_sound"
+SOUND_DIR = "./shade_sound"
 
 def color_text(text, color_code):
     return f"\033[{color_code}m{text}\033[0m"
